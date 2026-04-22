72c74a34af858a2794cbd9bf79a6795770fc6c62
zNettox <nett0x.br@gmail.com>
2026-04-22 16:35:50 -0400

    firebase via env vars

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCosCwNuQ4UkEvINGneV7YfN4YvveDBn0k",
-  authDomain: "socio-app-c26bc.firebaseapp.com",
-  projectId: "socio-app-c26bc",
-  storageBucket: "socio-app-c26bc.firebasestorage.app",
-  messagingSenderId: "734599420413",
-  appId: "1:734599420413:web:bdadd51c6d8ab33906a194",
-  measurementId: "G-QCXG3G13FJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
